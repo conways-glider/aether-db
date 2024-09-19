d2f6c3c5ac32862314f0eb1c75c32eb733df2a34
Nia <niawatts95@gmail.com>
2024-09-18 23:34:09 -0600

    Add comments

--- crates/db/src/main.rs
+++ crates/db/src/main.rs
@@ -10,7 +10,7 @@ use futures::{sink::SinkExt, stream::StreamExt};
 use store::DataStore;
 use std::{borrow::Cow, net::SocketAddr, ops::ControlFlow, sync::Arc};
 use tower_http::trace::{DefaultMakeSpan, TraceLayer};
-use tracing::{error, info};
+use tracing::{debug, error, info};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod store;
@@ -22,6 +22,8 @@ struct AppState {
 
 #[tokio::main]
 async fn main() {
+    // set up tracing subsciber
+    // this let's us get our logs
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
@@ -31,21 +33,23 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    // set up our app state
+    // this contains our runtime data and configs
     let app_state = Arc::new(AppState{ data_store: DataStore::default() });
 
+    // set up routing and middleware
     let app = Router::new()
         .route("/ws", get(ws_handler))
         .layer(TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::default()))
         .with_state(app_state);
 
-    // run it with hyper
+    // run the server
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(
         listener,
-        // app,
         app.into_make_service_with_connect_info::<SocketAddr>(),
     )
     .await
