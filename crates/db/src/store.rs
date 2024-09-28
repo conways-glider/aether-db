4b11426ff8e55e8cfcfe98cd492efe9e4470bec5
Nia <niawatts95@gmail.com>
2024-09-27 23:06:24 -0600

    Add database internal type

--- crates/db/src/store.rs
+++ crates/db/src/store.rs
@@ -1,15 +1,17 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
 use aether_common::BroadcastMessage;
 use dashmap::DashMap;
-use tokio::sync::broadcast;
+use tokio::{sync::broadcast, time::{sleep, Instant}};
+
+use crate::db::DatabaseValue;
 
 #[derive(Clone)]
 pub struct DataStore {
     // Data
     pub broadcast_channel: broadcast::Sender<BroadcastMessage>,
-    pub string_db: Arc<DashMap<String, String>>,
-    pub json_db: Arc<DashMap<String, serde_json::Value>>,
+    pub string_db: Arc<DashMap<String, DatabaseValue<String>>>,
+    pub json_db: Arc<DashMap<String, DatabaseValue<serde_json::Value>>>,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -29,23 +31,28 @@ impl Default for DataStore {
 }
 
 impl DataStore {
-    pub fn insert_string_db(&self, key: String, value: String) {
+    pub async fn handle_expiry(&self) {
+        let sleep_duration = Duration::from_secs(1);
+        loop {
+            todo!();
+        }
+    }
+
+    pub fn insert_string_db(&self, key: String, value: String,) {
+        let value = DatabaseValue{ value, expiry: None };
         self.string_db.insert(key, value);
     }
 
     pub fn insert_json_db(&self, key: String, value: serde_json::Value) {
+        let value = DatabaseValue{ value, expiry: None };
         self.json_db.insert(key, value);
     }
 
     pub fn get_string_db(&self, key: &str) -> Option<String> {
-        self.string_db.get(key).map(|e| e.clone())
+        self.string_db.get(key).map(|e| e.value.clone())
     }
 
     pub fn get_json_db(&self, key: &str) -> Option<serde_json::Value> {
-        self.json_db.get(key).map(|e| e.clone())
+        self.json_db.get(key).map(|e| e.value.clone())
     }
-
-    // pub fn get_json_db(&self, key: &str) {
-    //     self.json_db.get(key)
-    // }
 }
