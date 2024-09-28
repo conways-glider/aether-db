055e52b6910af8ce6e7dc4c618114cc7370c82dc
Nia <niawatts95@gmail.com>
2024-09-28 01:37:50 -0600

    Add doc comments

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -4,6 +4,9 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Command {
+    /// This subscribes your client to the given channel
+    ///
+    /// If `subscribe_to_self` is true, you will receive messages sent to channels that were sent by yourself.
     SubscribeBroadcast {
         channel: String,
 
@@ -11,16 +14,28 @@ pub enum Command {
         #[serde(default)]
         subscribe_to_self: bool,
     },
+
+    /// This unsubscribes your client from the given channel
     UnsubscribeBroadcast(String),
+
+    /// This sends a broadcast to the given channel
+    ///
+    /// If the channel is `general`, all clients will receive this message.
     SendBroadcast {
         channel: String,
         message: String,
     },
+
+    /// This sets a value in the string database
+    ///
+    /// `expiration` is the expiration time in seconds.
     SetString {
         key: String,
         value: String,
         expiration: Option<u32>,
     },
+
+    /// This retrieves a value from the string database
     GetString {
         key: String,
     },
@@ -30,8 +45,13 @@ pub enum Command {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Message {
+    /// This is sent at the opening of a websocket with your client id
     ClientId(String),
+
+    /// This contains broadcast messages sent to your subscriptions or to the `general` channel
     BroadcastMessage(BroadcastMessage),
+
+    /// This contains the result of a GetString command
     GetString(Option<String>),
 }
 
