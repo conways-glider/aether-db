65715aebab17a3b6689cbe43fcd5755dc589bdff
Nia <niawatts95@gmail.com>
2024-10-03 21:07:57 -0600

    Add expire_at to message value

--- crates/common/src/command.rs
+++ crates/common/src/command.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
 
 use crate::db::Data;
 
@@ -44,4 +45,19 @@ pub enum Command {
 pub struct Value {
     pub data: Data,
     pub expiry: Option<u32>,
+    #[serde(with = "time::serde::iso8601::option")]
+    pub expire_at: Option<OffsetDateTime>
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deserialization() {
+        let expected_value = Value { data: Data::Int(1), expiry: None, expire_at: Some(OffsetDateTime::from_unix_timestamp(1728043200).unwrap()) };
+        let input = "{\"data\":{\"int\":1},\"expiry\":null,\"expire_at\":\"2024-10-04T12:00:00Z\"}";
+        let value: Value = serde_json::from_str(input).unwrap();
+        assert_eq!(value.expire_at, expected_value.expire_at);
+    }
 }
